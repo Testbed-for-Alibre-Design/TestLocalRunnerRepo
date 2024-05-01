214bc92f3a8e71c7f07b73db5af8e579103d76e8
Stephen S. Mitchell <stephen@stephensmitchell.com>
2024-05-01 14:59:38 -0500

    Add new Console.WriteLine statement in Program.vb

--- Program.vb
+++ Program.vb
@@ -3,5 +3,6 @@ Module Program ' Declares the Program module.
     Sub Main(args As String()) ' Declares the Main subroutine, the entry point of the program, which can accept command-line arguments.
         Console.WriteLine("Hello World!") ' Outputs "Hello World!" to the console.
         Console.WriteLine("+36  : ISWORKING: YES") ' Outputs "+5" to the console.
+        Console.WriteLine("This is a test") ' Outputs "This is a test" to the console.
     End Sub ' Ends the Main subroutine.
 End Module ' Ends the Program module.
